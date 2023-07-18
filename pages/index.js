7dd850f21ef20b787859d68e5d6d5cd9d9b9463a
Yudha Haris Purnama <yudha@clade.ventures>
2023-07-18 21:05:31 +0700

    feat: configure homepage

--- pages/index.js
+++ pages/index.js
@@ -8,39 +8,73 @@ import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { asyncPreloadProcess } from "../states/preload/action";
 import { asyncUnsetAuthUser } from "../states/auth/action";
+import asyncPopulateThreads from "../states/shared/action";
 
 export default function Home() {
-  const { auth = null, preload = false } = useSelector((states) => states);
+  const {
+    auth = null,
+    preload = false,
+    threads = [],
+    users = [],
+  } = useSelector((states) => states);
 
   const dispatch = useDispatch();
   const router = useRouter();
 
   useEffect(() => {
     dispatch(asyncPreloadProcess());
+    dispatch(asyncPopulateThreads());
   }, [dispatch]);
 
   const onSignOut = () => {
     dispatch(asyncUnsetAuthUser());
   };
 
+  const onLike = ({ threadId, isLiked }) => {
+    if (auth) {
+      dispatch(asyncToggleLikeThread({ threadId, isLiked }));
+    } else {
+      router.push("/auth/login");
+    }
+  };
+
+  const onCreatePost = ({ title, content }) => {
+    if (auth) {
+      dispatch(asyncAddThread({ title, body: content }));
+    } else {
+      router.push("/auth/login");
+    }
+  };
+
+  const threadList = threads.map((thread) => ({
+    ...thread,
+    user: users.find((user) => user.id === thread.ownerId),
+    auth: auth === null ? null : auth.id,
+  }));
+
   if (preload) {
     return null;
-  }
-
-  if (!auth) {
-    router.push("/auth/login");
   }
 
   return (
     <>
       <div className="bg-slate-100 min-h-screen pt-16">
         <header>
-          <NavigationBar logout={onSignOut} onBackHome={() => {}} />
+          <NavigationBar
+            logout={onSignOut}
+            onBackHome={() => {
+              router.push("/");
+            }}
+          />
           {/* <Loading /> */}
         </header>
         <main>
-          <ForumInput post={() => {}} />
-          <ForumList threads={[]} like={() => {}} userId={""} />
+          <ForumInput post={onCreatePost} />
+          <ForumList
+            threads={threadList}
+            like={onLike}
+            userId={auth === null ? "" : auth.id}
+          />
         </main>
       </div>
 
